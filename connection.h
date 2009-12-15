439a00b146eec8f3164181c60178be84ff42ebff
Benjamin <fan2005@gmail.com>
2009-12-15 15:57:10 +0800

    add new file: connection.

---
+++ connection.h
@@ -0,0 +1,106 @@
+#ifndef _CONNECTION_H
+#define _CONNECTION_H
+
+#include "socket.h"
+
+
+#define CONN_BUF_SZ   4096
+
+
+struct ConnProp
+{
+    std::string remote_host;
+    uint32_t	remote_port;
+    uint32_t	timeout_secs;
+};
+
+
+class Connection : public uos::Socket
+{
+    friend class ConnPool;
+    enum CONN_STATE
+    {
+        S_FREE,
+        S_CONNECTING,
+        S_READING,
+        S_WRITING,
+        S_DONE
+    };
+public:
+    Connection(const ConnProp& prop);
+    ~Connection();
+
+    int do_read();
+    int do_write();
+    int do_connect();
+
+    void setState(uint32_t s)
+    {
+        _state = s;
+        _last_active_time = time(0);
+    }
+
+    bool isFree() const
+    {
+        return ( S_FREE == _state );
+    }
+
+    bool isConnecting() const
+    {
+        return ( S_CONNECTING == _state );
+    }
+
+    bool isReading() const
+    {
+        return ( S_READING == _state );
+    }
+
+    bool isWriting() const
+    {
+        return ( S_WRITING == _state );
+    }
+
+    bool isDone() const
+    {
+        return ( S_DONE == _state );
+    }
+
+    bool isTimeout() const
+    {
+        return ( time(0) > _last_active_time + _prop.timeout_secs );
+    }
+
+protected:
+    Connection();
+	void setProp(const ConnProp& prop)
+	{
+		_prop = prop;
+	}
+
+    int disconnect();
+
+private:
+	ConnProp	_prop;
+
+    time_t      _last_active_time;
+    uint32_t    _sequence;
+
+    char		_wr_buf[CONN_BUF_SZ];
+    char		_rd_buf[CONN_BUF_SZ];
+    uint32_t	_wr_size;
+    uint32_t	_rd_size;
+    uint32_t	_wr_idx;
+    uint32_t	_rd_idx;
+
+    uint32_t	_state;
+
+};
+
+
+
+#endif//(_CONNECTION_H)
+
+
+
+
+
--- connector.h
+++ connector.h
@@ -2,13 +2,11 @@
 #define _CONNECTOR_H
 
 #include "thread.h"
-#include "socket.h"
-#include "sockwatcher.h"
 #include "mutex.h"
+#include "sockwatcher.h"
+#include "connection.h"
 #include <map>
 
-
-#define CONN_BUF_SZ   4096
 
 struct ConnProperty
 {
@@ -19,96 +17,6 @@ struct ConnProperty
     int     rcv_timeout;    // receive msg timeout, by milliseconds.
     int     hb_interval;    // heartbeat interval, by seconds.
 };
-
-struct ConnProp
-{
-    std::string remote_host;
-    uint32_t	remote_port;
-    uint32_t	timeout_secs;
-};
-
-
-class Connector : public uos::Socket
-{
-    friend class ConnPool;
-    enum CONN_STATE
-    {
-        S_FREE,
-        S_CONNECTING,
-        S_READING,
-        S_WRITING,
-        S_DONE
-    };
-public:
-    Connector(const ConnProp& prop);
-    ~Connector();
-
-    int do_read();
-    int do_write();
-    int do_connect();
-
-    void setState(uint32_t s)
-    {
-        _state = s;
-        _last_active_time = time(0);
-    }
-
-    bool isFree() const
-    {
-        return ( S_FREE == _state );
-    }
-
-    bool isConnecting() const
-    {
-        return ( S_CONNECTING == _state );
-    }
-
-    bool isReading() const
-    {
-        return ( S_READING == _state );
-    }
-
-    bool isWriting() const
-    {
-        return ( S_WRITING == _state );
-    }
-
-    bool isDone() const
-    {
-        return ( S_DONE == _state );
-    }
-
-    bool isTimeout() const
-    {
-        return ( time(0) > _last_active_time + _prop.timeout_secs );
-    }
-
-protected:
-    Connector();
-	void setProp(const ConnProp& prop)
-	{
-		_prop = prop;
-	}
-
-    int disconnect();
-
-private:
-	ConnProp	_prop;
-
-    time_t      _last_active_time;
-    uint32_t    _sequence;
-
-    char		_wr_buf[CONN_BUF_SZ];
-    char		_rd_buf[CONN_BUF_SZ];
-    uint32_t	_wr_size;
-    uint32_t	_rd_size;
-    uint32_t	_wr_idx;
-    uint32_t	_rd_idx;
-
-    uint32_t	_state;
-
-};
-
 
 struct AuthMsg
 {
@@ -123,14 +31,12 @@ struct AuthMsg
 	int state;
 };
 
-
 typedef std::map<int, AuthMsg> MsgMap;
 typedef std::pair<int, AuthMsg> MsgPair;
 typedef MsgMap::iterator MsgIter;
 
 std::string ftxy4399_request_encode(const AuthMsg& msg);
 void ftxy4399_response_decode(std::string res, AuthMsg& msg);
-
 
 struct PfAuthCfg
 {
@@ -141,12 +47,10 @@ struct PfAuthCfg
     ConnProperty    conn_proper;
 };
 
-
-
-class ConnPool : public uos::Thread
+class Connector : public uos::Thread
 {
 public:
-    ConnPool(uint32_t size, const ConnProp& prop)
+    Connector(uint32_t size, const ConnProp& prop)
     {
         _conn_size = size;
         uint32_t max = 10;//_watch.getmaxfiles();
@@ -161,7 +65,7 @@ public:
         start();
     };
 
-    ~ConnPool(){}
+    ~Connector(){}
 
     int sendRequest(const AuthMsg& req_msg, uint32_t req_seq);
 	int recvResponse(AuthMsg& res_msg, uint32_t* res_seq);
